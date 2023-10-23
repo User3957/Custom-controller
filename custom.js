40a499a58b6850209047346a6c313c963c6605d3
User3957 <148494906+User3957@users.noreply.github.com>
2023-10-23 08:35:18 -0700

    done

--- custom.js
+++ custom.js
@@ -2,6 +2,7 @@ const custom = document.getElementById("customizer")
 const face = document.getElementById("face")
 
 function change(part, color) {
+    alert("hi!")
     var partgrab = document.getElementById(part);
     partgrab.style.backgroundImage = color + ".png"
 }
